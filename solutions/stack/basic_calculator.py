e104a5dc3c2d688e04bafdca8e1b29bf80cadd50
apple.rachanee <apple.rachanee@dominos.com.au>
2025-02-13 15:10:32 +0700

    Support advance calculator

--- solutions/stack/basic_calculator.py
+++ solutions/stack/basic_calculator.py
@@ -1,41 +1,62 @@
 # https://leetcode.com/problems/basic-calculator/
-class Solution: 
+class Solution:
+     
     def calculate(self, s: str) -> int:
-        stack = []
-        result, sign, number = 0, '+', ''
+        
+        def precedence(op: str) -> int:
+            if op == '(' or op == ')':
+                return 0
+            elif op == '+' or op == '-':
+                return 1
+            else:
+                return 2
+            
+        def apply_operator(a: int, op: str, b: int) -> int:
+            if op == '+':
+                return a + b
+            elif op == '-':
+                return a - b
+            elif op == '*':
+                return a * b
+            else:
+                return a // b
+                   
+        nums, ops = [], []
+        number = ''
         
         for char in s:            
             if char.isdigit():
                 number += char
             else:
                 if number:
-                    result = Solution.updateResult(result, sign, int(number))
+                    nums.append(int(number))
                     number = ''
                     
-                if char in '+-*/':
-                    sign = char
-                elif char == '(':
-                    stack.append(result)
-                    stack.append(sign)
-                    result, sign = 0, '+'
+                if char == '(':
+                    ops.append(char)
                 elif char == ')':
-                    sing = stack.pop()    
-                    result = Solution.updateResult(stack.pop(), sing, result)
+                    while (ops and ops[-1] != '('):
+                        num2 = nums.pop()
+                        num1 = nums.pop()
+                        op = ops.pop()
+                        nums.append(apply_operator(num1, op, num2))
+                    ops.pop()
+                elif char in '+-*/':
+                    while (ops and ops[-1] != '(' and precedence(ops[-1]) >= precedence(char)):
+                        num2 = nums.pop()
+                        num1 = nums.pop()
+                        op = ops.pop()
+                        nums.append(apply_operator(num1, op, num2))
+                    ops.append(char)
                     
         if number:
-            result = Solution.updateResult(result, sign, int(number))
+            nums.append(int(number))
 
-        return result
-    
-    @staticmethod
-    def updateResult(currentValue: int, sign: str, number: int) -> int:
-        if sign == '+':
-            return currentValue + number
-        elif sign == '-':
-            return currentValue - number
-        elif sign == '*':
-            return currentValue * number
-        elif sign == '/':
-            return currentValue // number
-    
+        while (ops):
+                num2 = nums.pop()
+                num1 = nums.pop()
+                op = ops.pop()
+                nums.append(apply_operator(num1, op, num2))
+                
+        return nums[0]
     
