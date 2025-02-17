8f48275d44d2156e9543c074a0028e887e57ec6a
apple.rachanee <apple.rachanee@dominos.com.au>
2025-02-17 12:55:55 +0700

    Enhance the code of topKFrequent

--- solutions/heap/k_most_frequent_elements.py
+++ solutions/heap/k_most_frequent_elements.py
@@ -9,22 +9,14 @@ import heapq
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         num_counts = {}
-        heap = []
-        result = []
-        
         for num in nums:
-            count = num_counts.get(num, 0) 
-            count += 1
-            num_counts[num] = count
-        
-        for number, count in num_counts.items():            
+            num_counts[num] = num_counts.get(num, 0) + 1
+
+        heap = []
+        for number, count in num_counts.items():
             if len(heap) < k:
                 heapq.heappush(heap, (count, number))
             elif count > heap[0][0]:
                 heapq.heapreplace(heap, (count, number))
-        
-        for _ in range(k):
-            count, number = heapq.heappop(heap)
-            result.append(number)
-                  
-        return result
+
+        return [item[1] for item in heap]
