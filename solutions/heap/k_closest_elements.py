0dca579d51ec2090a35e9f6809078c4230a3dc8f
apple.rachanee <apple.rachanee@dominos.com.au>
2025-02-17 15:11:47 +0700

    Use counter library

---
+++ solutions/heap/k_closest_elements.py
@@ -0,0 +1,6 @@
+# https://leetcode.com/problems/find-k-closest-elements/
+# https://algo.monster/liteproblems/658
+
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        
--- solutions/heap/top_k_frequent_elements.py
+++ solutions/heap/top_k_frequent_elements.py
@@ -1,17 +1,15 @@
 # https://leetcode.com/problems/top-k-frequent-elements/description/
 # https://algo.monster/liteproblems/347
 
-from typing import List
+from typing import List, Counter
 import heapq
 
 # Time complexity: O(nlogk)
 # Space complexity: O(n)
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        num_counts = {}
-        for num in nums:
-            num_counts[num] = num_counts.get(num, 0) + 1
-
+        num_counts = Counter(nums)
+        
         heap = []
         for number, count in num_counts.items():
             if len(heap) < k:
