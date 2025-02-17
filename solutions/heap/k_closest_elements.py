d970a262ef0b18248ee5fdbd3ab4f5803ee14f65
apple.rachanee <apple.rachanee@dominos.com.au>
2025-02-17 16:49:00 +0700

    Add k closest element

--- solutions/heap/k_closest_elements.py
+++ solutions/heap/k_closest_elements.py
@@ -1,6 +1,28 @@
 # https://leetcode.com/problems/find-k-closest-elements/
 # https://algo.monster/liteproblems/658
 
+from typing import List
+
+# time complexity: O(log(n - k) + k)
+# space complexity: O(k)
 class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        
+        def findStartingIndex(arr: List[int], k: int, x: int) -> int:
+            n = len(arr)
+            left, right = 0, n - k
+            while left < right:
+                mid = left + right // 2
+                # Check the distance from the x to the middle element and the element at mid + k position.
+                # If the element at mid is closer to x or equal in distance compared to the element at mid + k,
+                # we move the right pointer to mid. Otherwise, we adjust the left pointer to mid + 1.
+                if x - arr[mid] <= arr[mid + k] - x:
+                    right = mid
+                else:
+                    left = mid + 1        
+            return left
+    
+        starting_index = findStartingIndex(arr, k, x)
+        return arr[starting_index:starting_index + k]
+        
         
---
+++ tests/heap/test_k_closest_elements.py
@@ -0,0 +1,19 @@
+import pytest
+from solutions.heap.k_closest_elements import Solution
+
+@pytest.mark.parametrize("arr, k, x, expected", [
+    ([1, 2, 3, 4, 5], 4, 3, [1, 2, 3, 4]),
+    ([1, 2, 3, 4, 5], 4, -1, [1, 2, 3, 4]),
+    ([1, 2, 3, 4, 5], 4, 6, [2, 3, 4, 5]),
+    ([1, 2, 3, 4, 5], 2, 3, [2, 3]),
+    ([1, 2, 3, 4, 5], 3, 3, [2, 3, 4]),
+    ([1, 2, 3, 4, 5], 1, 3, [3]),
+    ([1, 2, 3, 4, 5], 5, 3, [1, 2, 3, 4, 5]),
+    ([1, 2, 3, 4, 5], 3, 2, [1, 2, 3]),
+    ([1, 2, 3, 4, 5], 3, 4, [3, 4, 5]),
+    ([1,1,2,3,4,5], 4, -1, [1, 1, 2, 3]),
+    ([1,1,2,3,4,5], 5, 10, [1, 2, 3, 4, 5]),
+])
+def test_findClosestElements(arr, k, x, expected):
+    sol = Solution()
+    assert sol.findClosestElements(arr, k, x) == expected
