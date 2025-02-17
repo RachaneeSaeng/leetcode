1e60414c49c7463d8c5ef28fe6f0caee51cc95c3
apple.rachanee <apple.rachanee@dominos.com.au>
2025-02-17 15:26:36 +0700

    Enhance the top K frequent words solution

--- solutions/heap/top_k_frequent_words.py
+++ solutions/heap/top_k_frequent_words.py
@@ -1,23 +1,30 @@
 # https://leetcode.com/problems/top-k-frequent-words/
 # https://algo.monster/problems/top_k_frequently_mentioned_keywords
 
-from typing import List
+from typing import Counter, List, Tuple
 import heapq
 
-# time complexity: O(n * log(n))
+# time complexity: O(nlogk)
 # space complexity: O(n)
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        word_counts = {}
-        for word in words:
-            word_counts[word] = word_counts.get(word, 0) + 1
+        word_counts = Counter(words)
 
         heap = []
         for word, count in word_counts.items():           
-            heapq.heappush(heap, (-count, word))
+            heapq.heappush(heap, (count, RevrseValue(word)))
+            if len(heap) > k:
+                heapq.heappop(heap) # if there are multiple words with the same frequency, the word with higher value will be popped first (z will be popped before a)
               
-        result = []
-        for _ in range(k):
-            result.append(heapq.heappop(heap)[1]) # words with the same frequency are ordered alphabetically when they are popped from the heap
+        res = []
+        while len(heap) > 0:
+            res.append(heapq.heappop(heap)[1].value)
             
-        return result
+        return res[::-1]
+    
+class RevrseValue:
+    def __init__(self, value):
+        self.value = value
+
+    def __lt__(self, other):
+        return self.value > other.value # to make word with higher value less than word with lower value
--- tests/heap/test_top_k_frequent_words.py
+++ tests/heap/test_top_k_frequent_words.py
@@ -8,8 +8,8 @@ def solution():
 def test_kth_most_frequent_words_1(solution):
     assert solution.topKFrequent(["i","love","leetcode","i","love","coding"], 2) == ["i", "love"]
     
-def test_kth_most_frequent_words_3(solution):
+def test_kth_most_frequent_words_2(solution):
     assert solution.topKFrequent(["i","love","leetcode","i","love","coding"], 3) == ["i", "love", "coding"]
     
-def test_kth_most_frequent_words_2(solution):
+def test_kth_most_frequent_words_3(solution):
     assert solution.topKFrequent(["the","day","is","sunny","the","the","the","sunny","is","is"], 4) == ["the","is","sunny","day"]
