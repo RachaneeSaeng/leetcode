c96943c1f81b3cf99a505eab7c8cd0b2b8a38077
apple.rachanee <apple.rachanee@dominos.com.au>
2025-02-21 11:06:11 +0700

    Add finding the first occurrence

---
+++ solutions/binary_search/first_occurance.py
@@ -0,0 +1,22 @@
+# https://algo.monster/problems/binary_search_duplicates
+
+from typing import List
+
+# time complexity: O(log(n))
+# space complexity: O(1)
+class Solution:
+    def findFirstOccurance(self, arr: List[int], k: int) -> int:
+        n = len(arr)
+        left, right = 0, n
+        first_occurance_idx = -1
+        while left <= right:
+            mid = (left + right) // 2
+            if arr[mid] == k:
+                right = mid - 1
+                first_occurance_idx = mid
+            elif arr[mid] > k:
+                right = mid - 1
+            else:
+                left = mid + 1
+                
+        return first_occurance_idx
