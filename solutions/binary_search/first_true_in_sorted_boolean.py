4cdd58cfcc5db5588eff5159618524757fa1cafb
apple.rachanee <apple.rachanee@dominos.com.au>
2025-02-21 10:55:13 +0700

    Add finding first true

---
+++ solutions/binary_search/first_true_in_sorted_boolean.py
@@ -0,0 +1,20 @@
+# https://algo.monster/problems/binary_search_boundary
+
+from typing import List
+
+# time complexity: O(log(n))
+# space complexity: O(1)
+class Solution:
+    def findFirstTrue(self, arr: List[bool]) -> int:
+        n = len(arr)
+        left, right = 0, n
+        first_true_idx = -1
+        while left <= right:
+            mid = (left + right) // 2
+            if arr[mid]:
+                right = mid - 1
+                first_true_idx = mid
+            else:
+                left = mid + 1
+                
+        return first_true_idx
