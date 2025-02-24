0644c3dabe7f3ddb5c621600b20dc923212ab786
apple.rachanee <apple.rachanee@dominos.com.au>
2025-02-24 12:17:28 +0700

    Add Find linked-list cycle

---
+++ solutions/two_pointers/find_linked_list_cycle.py
@@ -0,0 +1,27 @@
+# https://algo.monster/problems/linked_list_cycle
+# https://leetcode.com/problems/linked-list-cycle/description/
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+from typing import Optional
+from solutions.two_pointers.list_node import ListNode
+
+# time complexity: O(n)
+# space complexity: O(1)
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow = head
+        fast = head
+        
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            
+            if slow == fast:
+                return True
+        
+        return False
